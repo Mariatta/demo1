76bd7a8eccd456b968807a06a6c0fb30c5ffe7aa
Mariatta Wijaya <mariatta.wijaya@uplight.com>
2021-06-23 11:50:30 -0700

    Add the code to thank the person for installing the GH App.

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -49,7 +49,22 @@ async def webhook(request):
 @router.register("installation", action="created")
 async def repo_installation_added(event, gh, *args, **kwargs):
     installation_id = event.data["installation"]["id"]
-    pass
+    installation_access_token = await
+    apps.get_installation_access_token(
+        gh,
+        installation_id=installation_id,
+        app_id=os.environ.get("GH_APP_ID"),
+        private_key=os.environ.get("GH_PRIVATE_KEY")
+    )
+    url = event.data["repositories"][0]["full_name"]
+    response = await gh.post(
+        url,
+        data={
+            'title': 'Thanks for installing the app',
+            'body': 'Thanks!',
+        },
+        oauth_token=installation_access_token["token"]
+    )
 
 
 if __name__ == "__main__":  # pragma: no cover
